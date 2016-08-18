73a932d7d09dc3d8bdbe1dbc06eeb22ac456a9f3
Justin Dell <dell.justin@gmail.com>
2016-08-18 10:49:48 -0500

    improvements

--- lineup_generator.rb
+++ lineup_generator.rb
@@ -1,54 +1,60 @@
 require 'rglpk'
 require 'csv'
 
+NUM_LINEUPS = 50
+STACK_SIZE = 4
+OVERLAP = 4
+INCLUDED_TEAMS = %w(BOS BAL LAD PHI CLE CHW KCR DET MIA CIN MIN ATL MIL CHC OAK TEX ARI NYM SEA LAA)
+
 @players = []
 @stacks = []
 CSV.open('pitchers.csv', headers: true, header_converters: :symbol).each { |row| @players << row.to_hash }
 CSV.open('hitters.csv', headers: true, header_converters: :symbol).each { |row| @players << row.to_hash }
 @draftkings = CSV.open('draftkings.csv', headers: true, header_converters: :downcase).map { |row| row.to_hash }
-@players.select { |p| p[:pos] != 'SP' }.group_by { |p| p[:team] }.each { |_, p| @stacks += p.combination(3).to_a.sort_by { |s| s.inject(0) { |acc,p| acc += p[:fpts].to_f } }.take(25) }
-
-puts @stacks.count
+@players = @players.select { |p| p[:salary].to_f > 0 }
+@players = @players.select { |p| INCLUDED_TEAMS.include?(p[:team]) } unless INCLUDED_TEAMS.empty?
+@players = @players.sort_by { |p| p[:fpts].to_f }.reverse.take(250)
+@players.select { |p| p[:pos] != 'SP' }.group_by { |p| p[:team] }.each { |_, p| @stacks += p.combination(STACK_SIZE).to_a.sort_by { |s| s.inject(0) { |acc,p| acc += p[:fpts].to_f } }.reverse.take(10) }
+@stacks = @stacks.sort_by { |s| s.inject(0) { |acc,p| acc += p[:fpts].to_f } }.reverse.take(75)
+@zero_stacks = @stacks.map { 0 }
+@zero_players = @players.map { 0 }
 
 def create_new_lineup(lineups)
   problem = Rglpk::Problem.new
   problem.name = 'optimal lineup generator'
   problem.obj.dir = Rglpk::GLP_MAX
-  zero_stacks = @stacks.map { 0 }
-  zero_players = @players.map { 0 }
-
-  rows = problem.add_rows(9)
   matrix = []
 
+  rows = problem.add_rows(9)
   %w(C 1B 2B 3B SS).each_with_index do |position, i|
     rows[i].name = position
     rows[i].set_bounds(Rglpk::GLP_FX, 1, 1)
-    matrix += @players.map { |p| p[:pos] == position ? 1 : 0 } + zero_stacks.dup
+    matrix += @players.map { |p| p[:pos] == position ? 1 : 0 } + @zero_stacks.dup
   end
 
   rows[5].name = 'P'
   rows[5].set_bounds(Rglpk::GLP_FX, 2, 2)
-  matrix += @players.map { |p| p[:pos] == 'SP' ? 1 : 0 } + zero_stacks.dup
+  matrix += @players.map { |p| p[:pos] == 'SP' ? 1 : 0 } + @zero_stacks.dup
 
   rows[6].name = 'OF'
   rows[6].set_bounds(Rglpk::GLP_FX, 3, 3)
-  matrix += @players.map { |p| p[:pos] == 'OF' ? 1 : 0 } + zero_stacks.dup
+  matrix += @players.map { |p| p[:pos] == 'OF' ? 1 : 0 } + @zero_stacks.dup
 
   rows[7].name = 'Salary'
   rows[7].set_bounds(Rglpk::GLP_DB, 0, 50000)
-  matrix += @players.map { |p| p[:salary].to_i } + zero_stacks.dup
+  matrix += @players.map { |p| p[:salary].to_i } + @zero_stacks.dup
 
   rows[8].name = 'Team Size'
   rows[8].set_bounds(Rglpk::GLP_FX, 10, 10)
-  matrix += @players.map { 1 } + zero_stacks.dup
+  matrix += @players.map { 1 } + @zero_stacks.dup
 
   rows = problem.add_rows(@stacks.count)
   @stacks.each_with_index do |stack, i|
     rows[i].name = "Stack #{i}"
     rows[i].set_bounds(Rglpk::GLP_LO, 0, 0)
-    player_line = zero_players.dup
-    stack_line = zero_stacks.dup
-    stack.each { |p| player_line[@players.find_index { |i| i == p }] = 1/3.0 }
+    player_line = @zero_players.dup
+    stack_line = @zero_stacks.dup
+    stack.each { |p| player_line[@players.find_index { |i| i == p }] = 1/STACK_SIZE.to_f }
     stack_line[i] = -1
     matrix += player_line + stack_line
   end
@@ -56,14 +62,14 @@ def create_new_lineup(lineups)
   rows = problem.add_rows(1)
   rows[0].name = "One Stack"
   rows[0].set_bounds(Rglpk::GLP_LO, 1, 1)
-  matrix += zero_players.dup + @stacks.map { 1 }
+  matrix += @zero_players.dup + @stacks.map { 1 }
 
   if lineups.count > 0
     rows = problem.add_rows(lineups.count)
     lineups.each_with_index do |lineup, i|
       rows[i].name = "Overlap #{i}"
-      rows[i].set_bounds(Rglpk::GLP_DB, 0, 6)
-      matrix += lineup.take(@players.count) + zero_stacks.dup
+      rows[i].set_bounds(Rglpk::GLP_DB, 0, OVERLAP)
+      matrix += lineup + @zero_stacks.dup
     end
   end
 
@@ -72,7 +78,7 @@ def create_new_lineup(lineups)
   pitchers.each_with_index do |pitcher, i|
     rows[i].name = pitcher[:team]
     rows[i].set_bounds(Rglpk::GLP_DB, 0, 5)
-    matrix += @players.map { |p| p == pitcher ? 5 : (p[:position] != 'SP' && pitcher[:team] == p[:opp] ? 1 : 0) } + zero_stacks.dup
+    matrix += @players.map { |p| p == pitcher ? 5 : (p[:position] != 'SP' && pitcher[:team] == p[:opp] ? 1 : 0) } + @zero_stacks.dup
   end
 
   cols = problem.add_cols(@players.count + @stacks.count)
@@ -81,13 +87,13 @@ def create_new_lineup(lineups)
     c.kind = Rglpk::GLP_IV
   end
 
-  problem.obj.coefs = @players.map { |p| p[:fpts].to_f } + zero_stacks.dup
+  problem.obj.coefs = @players.map { |p| p[:fpts].to_f } + @zero_stacks.dup
 
   problem.set_matrix(matrix)
 
   problem.mip(presolve: Rglpk::GLP_ON)
   score = problem.obj.mip
-  return score, problem.cols.map { |c| c.mip_val }
+  return score, problem.cols.map { |c| c.mip_val }.take(@players.count)
 end
 
 def print_lineup(lineup, score)
@@ -95,26 +101,16 @@ def print_lineup(lineup, score)
   puts "SCORE: #{score}"
   puts "POS\tSALARY\tTEAM\tOPP\tPOINTS\tPLAYER"
   lineup.each_with_index do |solution, i|
-    puts "#{@players[i][:pos]}\t#{@players[i][:salary]}\t#{@players[i][:team]}\t#{@players[i][:opp]}\t#{@players[i][:fpts]}\t#{@players[i][:player]}" if solution == 1 && i < @players.count
+    puts "#{@players[i][:pos]}\t#{@players[i][:salary]}\t#{@players[i][:team]}\t#{@players[i][:opp]}\t#{@players[i][:fpts]}\t#{@players[i][:player]}" if solution == 1
   end
 end
 
 lineups = []
-score, lineup = create_new_lineup(lineups)
-lineups << lineup
-print_lineup(lineup, score)
-score, lineup = create_new_lineup(lineups)
-lineups << lineup
-print_lineup(lineup, score)
-score, lineup = create_new_lineup(lineups)
-lineups << lineup
-print_lineup(lineup, score)
-score, lineup = create_new_lineup(lineups)
-lineups << lineup
-print_lineup(lineup, score)
-score, lineup = create_new_lineup(lineups)
-lineups << lineup
-print_lineup(lineup, score)
+NUM_LINEUPS.times do
+  score, lineup = create_new_lineup(lineups)
+  lineups << lineup
+  print_lineup(lineup, score)
+end
 
 def lookup_fanduel_id(team, position)
   team.select { |p| p[:pos] == position }.map do |p|
@@ -138,7 +134,7 @@ file << "\n"
 lineups.each do |lineup|
   team = []
   lineup.each_with_index do |solution, i|
-    team << @players[i] if solution == 1 && i < @players.count
+    team << @players[i] if solution == 1
   end
   file << %w(SP C 1B 2B 3B SS OF).map { |pos| lookup_draftkings_id(team, pos) }.flatten.join(',')
   file << "\n"
